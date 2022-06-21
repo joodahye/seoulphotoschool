9224cec74f0e1be3bb3ea1bd57eaa4173892c713
joodahye <106372964+joodahye@users.noreply.github.com>
2022-06-21 12:30:43 +0900

    Add files via upload

--- js/seoulphotoschool.js
+++ js/seoulphotoschool.js
@@ -12,4 +12,20 @@ $("header li").on("mouseenter click",function(){
 		$(this).children("nav").slideUp();
 	});
 	
+	//포트폴리오:썸네일 클릭하면 큰이미지 보인다.
+	$(".photo").click(function(){
+		//<a>속 이미지 주소와 <img alt> 가져온다.
+		const p1 = $(this).children().attr("src");
+		const p2 = $(this).children().attr("alt");
+		console.log(p1 + p2);
+		
+		$(".big img").attr({"src": p1 , "alt" : p2});
+		$(".big").fadeIn();
+	});
+	
+	//포트폴리오 큰이미지 클릭하면 사라진다.
+	$(".big").click(function(){
+		$(this).fadeOut();
+	});
+	
 });
